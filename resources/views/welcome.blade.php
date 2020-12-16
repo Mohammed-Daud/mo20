f13edff72687eaab698662ea865617df835fb270
Mohammed.Daud <daud.csbt@gmail.com>
2020-12-16 17:04:19 +0530

    \p

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L8 - TODO</title>
+</head>
+<body>
+    <ul>
+        <li>Authentication</li>
+        <li>email verifications</li>
+        <li>Multi Gaurd Authentication</li>
+        <li>Authorization</li>
+        <li>Social Media login</li>
+        <li>URL Shortner</li>
+        <li>Multi Language</li>
+        <li>CRUD</li>
+        <li>Default Pagination</li>
+        <li>Default Custom Pagination</li>
+        <li>Data Tables</li>
+        <li>Multi DB</li>
+    </ul>
+</body>
+</html>
